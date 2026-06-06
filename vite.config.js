813c7e2dfb02fb20b39c00815282933f73841f8b
sistemasciopaer <sistemas.ciopaer@gmail.com>
2026-06-06 13:33:24 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/cin-ciopaer/',
   resolve: {
-    alias: { '@': '/src' }
+    alias: { '@': path.resolve(__dirname, './src') }
   }
 })
